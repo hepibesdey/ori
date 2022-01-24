f93b19c50b11d696e37d12a8deecf6440f1c4568
hepibesdey <hkwang127@gmail.com>
2022-01-24 19:00:28 +0700

    ketiga

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const words = ["kuliahnya lancar", "tambah dewasa", "tambah keren", "panjang umu
 let cursor = gsap.to(".cursor", { opacity: 0, ease: "power2.inOut", repeat: -1 });
 let boxTl = gsap.timeline();
 boxTl
-  .to(".box", { duration: 1, width: "30vw", delay: 0.5, ease: "power4.inOut" })
+  .to(".box", { duration: 1, width: "65vw", delay: 0.5, ease: "power4.inOut" })
   //   .from(".h1", { duration: 1, y: "7vw", ease: "power3.out" })
   .from(".hi", { duration: 1, y: "7vw", ease: "power3.out", onComplete: () => masterTl.play() })
   .to(".box", { duration: 1, height: "7vw", ease: "elastic.out" })
